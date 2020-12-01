3452024692f14325f5dd720d6411106c3eaaf7d3
jgandre <jagandre@gmail.com>
2020-12-01 07:19:36 +0000

    PyBank analysis writtent o text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,9 +16,9 @@ with open(path, "r") as file:
 #variables (start at 0)
 # months = []
 prof_loss = 0
-avg_change_pl = []
-greatest_increase = []
-greatest_deacrease = []
+avg_change_pl = 0
+greatest_increase = 0
+greatest_deacrease = 0
 total_pl = 0
 
 #print(data)
@@ -30,6 +30,17 @@ for row in data:
     total_pl += pl 
 
 format_total_pl = "${:,.2f}".format(total_pl)    
+
+with open("Analysis/PyBank.txt", "w+") as file:
+    file.write("Financial Analysis\n")
+    file.write("______________________________\n")
+    file.write(f"Total Months: {total_months}\n")
+    file.write(f"Total: {format_total_pl}\n")
+    file.write(f"Average Change: \n")
+    file.write(f"Greatest Increase in Profits: \n")
+    file.write(f"Greatest Decrease in Profits: \n")
+    
+    
     
        
         
